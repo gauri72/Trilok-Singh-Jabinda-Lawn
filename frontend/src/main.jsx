c74ad3d4f7af66c0418a28c8b728eda173550bfd
Your Name <your-email@example.com>
2025-09-10 12:20:16 +0530

    Update Our Services section: add background image, update card shapes, add green border hover, update arrow shapes and colors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import LandshaperNavbar from './components/LandshaperNavbar'
 import HeroSection from './components/HeroSection'
+import OurServices from './components/OurServices'
 
 function App() {
   return (
     <>
       <LandshaperNavbar />
       <HeroSection />
+      <OurServices />
       <main style={{paddingTop: 0}}>
         {/* Additional sections will go here */}
       </main>
